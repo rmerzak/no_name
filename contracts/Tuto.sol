25c97341545082da32269f140017858a7cb1bbb5
Rabi Merzak <rmerzak@e3r6p2.1337.ma>
2022-07-24 18:17:40 +0100

    first push

---
+++ contracts/Tuto.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Tuto {
+    // Storage variable.
+    uint public funds = 1000;
+    int public counter = -10;
+}
---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+  },
+
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.14",      // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
+};
